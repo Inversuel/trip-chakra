beffb2d7584750ad9439c2fee76d221f81de5938
Karol Maj <invers1996@gmail.com>
2024-02-19 19:42:47 +0100

    fix fetch api call on production

--- src/fetch/fetch.ts
+++ src/fetch/fetch.ts
@@ -3,15 +3,15 @@ import { Trip } from "./fetch.type";
 import { TripDetails } from "./fetchDetails.type";
 import { cache } from "react";
 
-const BASEURL = process.env.NEXT_PUBLIC_URL;
+const BASEURL = '/api/trip/'
 
 export const fetchTrip = cache(async ({ pageParam = 1 }) => {
   const res = await axios<Trip>(
-    `${BASEURL}/api/trip/?_page=${pageParam}&_per_page=10`
+    `${BASEURL}?_page=${pageParam}&_per_page=10`
   );
   return res;
 });
 export const fetchTripDetails = cache(async (id: string) => {
-  const res = await axios<TripDetails>(`${BASEURL}/api/trip/${id}`);
+  const res = await axios<TripDetails>(`${BASEURL}${id}`);
   return res;
 });
